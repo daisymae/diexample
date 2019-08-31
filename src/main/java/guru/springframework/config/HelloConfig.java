d54255f3c85844fdc6d1c610c7a6669fd2c98eac
daisymae <daisymae.orcadiafarm@gmail.com>
2019-08-31 16:37:43 -0500

    Primary
    Adding the @Primary annotation will pick up the primary bean to match the profile, else, will error out that there are multiple beans that match. HOWEVER, if change the active profile to spanish, will give that same error, even though @Primary is in use.

--- src/main/java/guru/springframework/config/HelloConfig.java
+++ src/main/java/guru/springframework/config/HelloConfig.java
@@ -1,23 +1,53 @@
 package guru.springframework.config;
 
+import guru.springframework.service.HelloWorldFactory;
 import guru.springframework.service.HelloWorldService;
 import guru.springframework.service.HelloWorldServiceEnglishImpl;
 import guru.springframework.service.HelloWorldSpanishImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class HelloConfig {
+
+  @Bean
+  public HelloWorldFactory helloWorldFactory() {
+    return new HelloWorldFactory();
+  }
+
   @Bean
   @Profile("english")
-  public HelloWorldService helloWorldServiceEnglish() {
-    return new HelloWorldServiceEnglishImpl();
+  @Primary
+  public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory) {
+    return factory.createHelloWorldService("en");
   }
 
   @Bean
   @Profile("spanish")
-  public HelloWorldService helloWorldServiceSpanish() {
-    return new HelloWorldSpanishImpl();
+  public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory) {
+    return factory.createHelloWorldService("es");
   }
+
+  @Bean
+  public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory) {
+    return factory.createHelloWorldService("fr");
+  }
+
+  @Bean
+  public HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory) {
+    return factory.createHelloWorldService("de");
+  }
+
+  @Bean
+  public HelloWorldService helloWorldServicePolish(HelloWorldFactory factory) {
+    return factory.createHelloWorldService("pl");
+  }
+
+  @Bean
+  public HelloWorldService helloWorldServiceRussian(HelloWorldFactory factory) {
+    return factory.createHelloWorldService("ru");
+  }
+
 }
