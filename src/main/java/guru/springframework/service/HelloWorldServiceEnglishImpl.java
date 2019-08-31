9de1876da0d27ed60c9121e2e6bab34f8c7c95c4
daisymae <daisymae.orcadiafarm@gmail.com>
2019-08-31 15:34:51 -0500

    Profiles
    Set up profiles on the beans and specify the active profile in properties. Now Spring knows which one to use.

--- src/main/java/guru/springframework/service/HelloWorldServiceEnglishImpl.java
+++ src/main/java/guru/springframework/service/HelloWorldServiceEnglishImpl.java
@@ -1,8 +1,10 @@
 package guru.springframework.service;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("english")
 public class HelloWorldServiceEnglishImpl implements HelloWorldService {
 
   @Override
--- src/main/java/guru/springframework/service/HelloWorldSpanishImpl.java
+++ src/main/java/guru/springframework/service/HelloWorldSpanishImpl.java
@@ -1,8 +1,10 @@
 package guru.springframework.service;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("spanish")
 public class HelloWorldSpanishImpl implements HelloWorldService {
 
   @Override
