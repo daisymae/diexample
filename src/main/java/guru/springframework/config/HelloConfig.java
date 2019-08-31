fea8eca481b0a5abc4c6fa7b546dabd78317add9
daisymae <daisymae.orcadiafarm@gmail.com>
2019-08-31 16:01:14 -0500

    Profiles
    Now works with profiles set.

--- src/main/java/guru/springframework/config/HelloConfig.java
+++ src/main/java/guru/springframework/config/HelloConfig.java
@@ -5,15 +5,18 @@ import guru.springframework.service.HelloWorldServiceEnglishImpl;
 import guru.springframework.service.HelloWorldSpanishImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class HelloConfig {
   @Bean
+  @Profile("english")
   public HelloWorldService helloWorldServiceEnglish() {
     return new HelloWorldServiceEnglishImpl();
   }
 
   @Bean
+  @Profile("spanish")
   public HelloWorldService helloWorldServiceSpanish() {
     return new HelloWorldSpanishImpl();
   }
